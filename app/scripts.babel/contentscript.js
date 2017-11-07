78296835d5a7331fefa902a23f8a71fc615b2e72
Anand Shenoy <anandshenoy@Anands-MacBook-Pro.local>
2017-11-06 23:15:51 -0800

    :truck: trying to publish the app for the first iteration

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+var keys = [];
+var isShiftGPressed = function(){
+    if(keys.length === 2) {
+        if(keys[0] !== keys[1]){
+            openNewTabWithSearchTerm(window.getSelection().toString());
+        }else{
+            keys.pop();
+        }
+        emptyKeys();  
+    }
+};
+var openNewTabWithSearchTerm = function(query){
+    var guglSearchUrl = 'https://www.google.com/search?q=';
+    window.open(guglSearchUrl + query, '_blank');
+}
+var emptyKeys = function(){
+    while(keys.length > 0) {
+        keys.pop();
+    }
+};
+var handleKeyDown = function(e){
+        if(e.keyCode === 16 || e.keyCode === 71){
+            keys.push(e.keyCode);
+            if(keys.length === 2) {
+                isShiftGPressed();
+            }
+        }
+};
+
+window.addEventListener('keydown',handleKeyDown,true);
