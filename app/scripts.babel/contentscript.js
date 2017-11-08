0699317089079c1a2e971bf0a3a24d3c4aad6033
Anand Shenoy <anandshenoy@Anands-MacBook-Pro.local>
2017-11-08 01:12:04 -0800

    :rocket: finishing the initial release with bit of history

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -13,6 +13,24 @@ var isShiftGPressed = function(){
 };
 var openNewTabWithSearchTerm = function(query){
     var guglSearchUrl = 'https://www.google.com/search?q=';
+    var localArrayOfUrlsSearched, localArrayOfTermsSearched;
+    chrome.storage.sync.get(['searchUrlsOnGuglitv5','searchTermsOnGuglitv5'], function(items) {
+        localArrayOfUrlsSearched = items.searchUrlsOnGuglitv5;
+        localArrayOfTermsSearched = items.searchTermsOnGuglitv5;
+        if(localArrayOfUrlsSearched === null || localArrayOfUrlsSearched === undefined){
+            localArrayOfUrlsSearched = '';
+        }
+        if(localArrayOfTermsSearched === null || localArrayOfTermsSearched === undefined){
+            localArrayOfTermsSearched = '';
+        }
+        localArrayOfUrlsSearched = localArrayOfUrlsSearched + guglSearchUrl + query + '<<>>';
+        localArrayOfTermsSearched = localArrayOfTermsSearched + query + '<??>';
+        chrome.storage.sync.set({'searchUrlsOnGuglitv5': localArrayOfUrlsSearched,'searchTermsOnGuglitv5':localArrayOfTermsSearched}, function() {
+            // Notify that we saved.
+            message('Settings saved');
+        });
+    });
+    
     window.open(guglSearchUrl + query, '_blank');
 }
 var emptyKeys = function(){
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+    console.log('Reading the list');
+    var arrayOfUrls, arrayOfSearchItems;
+    chrome.storage.sync.get(['searchUrlsOnGuglitv5','searchTermsOnGuglitv5'], function(items) {
+        console.log('The type of items is '+typeof items);
+        //console.log('items '+JSON.stringify(items));
+        arrayOfUrls = items.searchUrlsOnGuglitv5.split('<<>>');
+        arrayOfSearchItems = items.searchTermsOnGuglitv5.split('<??>');
+        var ul = document.getElementById('listOfUrls');
+        for(var i=0;i< arrayOfUrls.length - 1;i++){
+            var li = document.createElement('li');
+            var hyperlink = document.createElement('a');
+            hyperlink.setAttribute('href',arrayOfUrls[i]);
+            hyperlink.appendChild(document.createTextNode(arrayOfSearchItems[i]));
+            li.appendChild(hyperlink);
+            ul.appendChild(li);
+        }
+        if(arrayOfUrls.length > 0){
+            var listOfUrlsTextPlaceholderEl = document.getElementById('lblOfList');
+            listOfUrlsTextPlaceholderEl.appendChild(document.createTextNode('Here is the list of URLS Searched : '));
+        }
+    });
